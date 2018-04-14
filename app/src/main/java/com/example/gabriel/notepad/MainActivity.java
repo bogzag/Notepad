fd8dfd93a417668f4e493a62ee091d526d7b35b5
Gabriel Bogza <gabriel.bogza98@gmail.com>
2018-04-14 20:58:59 +0300

    Initial commit

---
+++ app/src/main/java/com/example/gabriel/notepad/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.gabriel.notepad;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+
+public class MainActivity extends AppCompatActivity {
+    String TAG = MainActivity.class.getSimpleName();
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG,"Log, the app has started ");
+    }
+
+}
+
